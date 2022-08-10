545128bee7c641f0dd2fe6ff0706ed90400beac8
¨Vinícius <vini.ceriotti@hotmail.com>
2022-08-10 14:54:27 -0300

    DTO package and ClientDTO

---
+++ backend/src/main/java/com/ceriotti/customers/dto/ClientDTO.java
@@ -0,0 +1,100 @@
+package com.ceriotti.customers.dto;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.ceriotti.customers.entities.Client;
+
+public class ClientDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant birthDate;
+	private Integer children;
+	
+	public ClientDTO() {
+	}
+
+	public ClientDTO(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birthDate = birthDate;
+		this.children = children;
+	}
+	
+	// Criando esse construtor conseguimos povoar o ClientDTO apenas passando a entidade como argumento
+	public ClientDTO(Client entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.cpf = entity.getCpf();
+		this.income = entity.getIncome();
+		this.birthDate = entity.getBirthDate();
+		this.children = entity.getChildren();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+	
+	
+	
+}
--- backend/src/main/java/com/ceriotti/customers/services/ClientService.java
+++ backend/src/main/java/com/ceriotti/customers/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.ceriotti.customers.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ceriotti.customers.dto.ClientDTO;
 import com.ceriotti.customers.entities.Client;
 import com.ceriotti.customers.repositories.ClientRepository;
 
@@ -17,8 +19,18 @@ public class ClientService {
 	
 	@Transactional(readOnly = true) // Transactional garante que o método irá executar em uma transação com o banco de dados e o
 									// "readOnly = true" evita o locking no banco de dados, assim melhorando a performance
-	public List<Client> findAll() {
-		return repository.findAll();
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll(); // Repository não mexe com DTO, apenas com a entidade. Assim, declara-se uma lista do tipo "Client"
+		
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+		
+		// Conversão de lista do tipo Client para o tipo ClientDTO
+		// List<ClientDTO> listDto = new ArrayList<>() { 
+		// for(Client cli : list) {
+		//		listDto.add(new ClientDTO(cli));
+		// }
+		// return listDto;
+		
 	}
 	
 }
